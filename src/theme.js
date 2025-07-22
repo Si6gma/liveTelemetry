73b44b16ffa132f63235d165c2898902fd412fe4
Si6gma <neiv.malaviya@gmail.com>
2025-07-22 20:39:57 +0530

    Changed to ploty V1

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,10 @@
-// theme.js
 export const colors = {
-  rpm: "#4f46e5", // Indigo
-  throttle: "#22c55e", // Green
-  voltage: "#facc15", // Yellow
+  rpm: "rgb(99, 102, 241)", // Indigo
+  throttle: "rgb(34, 197, 94)", // Green
+  voltage: "rgb(234, 179, 8)", // Yellow
+  coolant_temp: "rgb(59, 130, 246)", // Blue
+  fuel_level: "rgb(251, 191, 36)", // Amber
+  lambda: "rgb(16, 185, 129)", // Emerald
+  oil_pressure: "rgb(244, 63, 94)", // Rose
+  oil_temp: "rgb(202, 138, 4)", // Amber Dark
 };
